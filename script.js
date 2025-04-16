764b31395c4eafc16fb4e7e7b96e44ad46c28047
Mario Azevedo <mariobct@msn.com>
2025-04-16 18:45:25 -0300

    JS da bolinha

--- script.js
+++ script.js
@@ -22,7 +22,17 @@ horaAtual()
 setInterval(horaAtual, 1000)
 
 
+const bolinha = document.querySelector('.bolinha');
 
+function atualizarBolinha() {
+    const agora = new Date()
+    const segundos = agora.getSeconds()
+    const angulo = segundos * 6
 
+    bolinha.style.transform = `translateX(-50%) rotate(${angulo}deg) translateY(-280px)`;
+}
+
+setInterval(atualizarBolinha, 1000)
+atualizarBolinha()
 
 
