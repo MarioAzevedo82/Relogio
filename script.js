309fcb0343c3c93e7f3dd6d18270623b17a601ea
Mario Azevedo <mariobct@msn.com>
2025-04-16 18:32:08 -0300

    Digital sem ponteiros

--- script.js
+++ script.js
@@ -21,21 +21,8 @@ function horaAtual(){
 horaAtual()
 setInterval(horaAtual, 1000)
 
-const marcadoresContainer = document.querySelector('.marcadores');
 
-for(let i = 0; i < 60; i++){
-    const marcador = document.createElement('div');
-    marcador.classList.add('marcador')
 
-    const angulo = i * 6;
-    marcador.style.transform = `rotate(${angulo}deg) translateY(-295px)`;
 
-    if (i % 5 === 0) {
-        marcador.style.height = "20px";
-        marcador.style.backgroundColor = "#fff";
-        marcador.style.width = '3px'
-    }
-    marcadoresContainer.appendChild(marcador);
-}
 
 
